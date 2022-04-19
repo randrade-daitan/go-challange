0ff487b88a8da85e950ca98cf02d954e139f7b21
Rodrigo Andrade <randrade@daitan.com>
2022-04-19 18:12:41 -0300

    d: Added ORM tests

--- internal/orm/orm_test.go
+++ internal/orm/orm_test.go
@@ -1,8 +1,14 @@
 package orm
 
 import (
+	"challange/internal/repository"
+	"database/sql"
 	"os"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func TestOrmDSN(t *testing.T) {
@@ -15,4 +21,82 @@ func TestOrmDSN(t *testing.T) {
 	if dsn != expectedDSN {
 		t.Errorf("orm dsn is incorrect")
 	}
+}
+
+func TestGetAllTasks(t *testing.T) {
+	repo := repository.RepositoryForTesting(testableOrm, t)
+	repo.TestRepositoryGetTasks("SELECT * FROM \"task\"", t)
+}
+
+func TestGetTaskByID(t *testing.T) {
+	repo := repository.RepositoryForTesting(testableOrm, t)
+	repo.TestRepositoryGetTaskById("SELECT * FROM \"task\" WHERE id = $1 ORDER BY \"task\".\"id\" LIMIT 1", t)
+}
+
+func TestAddTask(t *testing.T) {
+	task := repository.Task{
+		ID:        30,
+		Name:      "ORM Test",
+		Completed: true,
+	}
+	repo := repository.RepositoryForTesting(testableOrm, t)
+
+	repo.Mock.MatchExpectationsInOrder(false)
+	repo.Mock.ExpectBegin()
+	repo.Mock.
+		ExpectQuery("INSERT INTO \"task\" (\"name\",\"completed\",\"id\") VALUES ($1,$2,$3) RETURNING \"id\"").
+		WithArgs(task.Name, task.Completed, task.ID).
+		WillReturnRows(repo.Mock.NewRows([]string{"id"}).AddRow(task.ID))
+	repo.Mock.ExpectCommit()
+
+	repo.TestRepositoryAddTask(task, t)
+}
+
+func TestEditTask(t *testing.T) {
+	task := repository.Task{
+		ID:        2,
+		Name:      "edited",
+		Completed: true,
+	}
+	repo := repository.RepositoryForTesting(testableOrm, t)
+
+	repo.Mock.ExpectBegin()
+	repo.Mock.
+		ExpectExec("UPDATE \"task\" SET \"name\"=$1,\"completed\"=$2 WHERE \"id\" = $3").
+		WithArgs(task.Name, task.Completed, task.ID).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	repo.Mock.ExpectCommit()
+
+	repo.TestRepositoryEditTask(task, t)
+}
+
+func TestGetTasksByCompletion(t *testing.T) {
+	q := "SELECT * FROM \"task\" WHERE completed = $1"
+
+	t.Run("fetch completed tasks", func(t *testing.T) {
+		repo := repository.RepositoryForTesting(testableOrm, t)
+		repo.TestRepositoryGetTasksByCompletion(q, t, true)
+	})
+
+	t.Run("fetch incompleted tasks", func(t *testing.T) {
+		repo := repository.RepositoryForTesting(testableOrm, t)
+		repo.TestRepositoryGetTasksByCompletion(q, t, false)
+	})
+}
+
+func testableOrm(db *sql.DB, t *testing.T) repository.Repository {
+	t.Helper()
+
+	dialector := postgres.New(postgres.Config{
+		DSN:                  "sqlmock_db_0",
+		DriverName:           "postgres",
+		Conn:                 db,
+		PreferSimpleProtocol: true,
+	})
+	ormDB, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		t.Fatalf("error creating new mock: %v", err)
+	}
+
+	return Orm{ormDB}
 }
--- internal/repository/database_test.go
+++ internal/repository/database_test.go
@@ -1,138 +1,61 @@
 package repository
 
 import (
-	"fmt"
-	"math/rand"
+	"database/sql"
 	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
 func TestGetAllTasks(t *testing.T) {
-	database, mock, rows := newMockDatabase(t)
-	defer database.Close()
-
-	rowCount := addRandomRows(rows, Task{0, "", true})
-	mock.
-		ExpectQuery("SELECT * FROM task").
-		WillReturnRows(rows)
-
-	tasks, err := database.GetAllTasks()
-
-	if err != nil {
-		t.Errorf("could not fetch all tasks: %v", err)
-	}
-	if len(tasks) != rowCount {
-		t.Errorf("unexpected number of tasks")
-	}
+	repo := RepositoryForTesting(testableDatabase, t)
+	repo.TestRepositoryGetTasks("SELECT * FROM task", t)
 }
 
 func TestGetTaskByID(t *testing.T) {
-	database, mock, rows := newMockDatabase(t)
-
-	rows.AddRow(15, "task", false)
-	mock.
-		ExpectQuery("SELECT * FROM task WHERE id = ?").
-		WillReturnRows(rows)
-
-	task, err := database.GetTaskByID(15)
-
-	if err != nil {
-		t.Errorf("could not fetch task by id: %v", err)
-	}
-	if task.ID != 15 {
-		t.Errorf("unexpected task id: %v", task.ID)
-	}
+	repo := RepositoryForTesting(testableDatabase, t)
+	repo.TestRepositoryGetTaskById("SELECT * FROM task WHERE id = ?", t)
 }
 
 func TestAddTask(t *testing.T) {
-	task := Task{10, "Title", false}
-	database, mock, _ := newMockDatabase(t)
+	task := Task{10, "DB Test", false}
+	repo := RepositoryForTesting(testableDatabase, t)
 
-	mock.
+	repo.Mock.
 		ExpectExec("INSERT INTO task (name, completed) VALUES (?, ?)").
 		WithArgs(task.Name, task.Completed).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+		WillReturnResult(sqlmock.NewResult(task.ID, 1))
 
-	id, err := database.AddTask(task)
-
-	if err != nil {
-		t.Errorf("could not add task: %v", err)
-	}
-	if id != 1 {
-		t.Errorf("unexpected task id: %v", id)
-	}
+	repo.TestRepositoryAddTask(task, t)
 }
 
 func TestEditTask(t *testing.T) {
 	task := Task{2, "edited", true}
-	database, mock, rows := newMockDatabase(t)
+	repo := RepositoryForTesting(testableDatabase, t)
 
-	rows.AddRow(2, "task", false)
-	mock.
+	repo.Mock.
 		ExpectExec("UPDATE task SET name = ?, completed = ? WHERE id = ?").
 		WithArgs(task.Name, task.Completed, task.ID).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+		WillReturnResult(sqlmock.NewResult(0, 1))
 
-	err := database.EditTask(task)
-	if err != nil {
-		t.Errorf("could not add task: %v", err)
-	}
+	repo.TestRepositoryEditTask(task, t)
 }
 
 func TestGetTasksByCompletion(t *testing.T) {
+	q := "SELECT * FROM task WHERE completed = ?"
+
 	t.Run("fetch completed tasks", func(t *testing.T) {
-		testFetchTestsByCompletion(t, true)
+		repo := RepositoryForTesting(testableDatabase, t)
+		repo.TestRepositoryGetTasksByCompletion(q, t, true)
 	})
 
 	t.Run("fetch incompleted tasks", func(t *testing.T) {
-		testFetchTestsByCompletion(t, false)
+		repo := RepositoryForTesting(testableDatabase, t)
+		repo.TestRepositoryGetTasksByCompletion(q, t, false)
 	})
 }
 
-func newMockDatabase(t *testing.T) (*Database, sqlmock.Sqlmock, *sqlmock.Rows) {
+func testableDatabase(db *sql.DB, t *testing.T) Repository {
 	t.Helper()
-
-	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	rows := sqlmock.NewRows([]string{"id", "name", "completed"})
-	return &Database{db}, mock, rows
-}
-
-func testFetchTestsByCompletion(t *testing.T, isCompleted bool) {
-	t.Helper()
-	database, mock, rows := newMockDatabase(t)
-
-	rowCount := addRandomRows(rows, Task{0, "", isCompleted})
-	mock.
-		ExpectQuery("SELECT * FROM task WHERE completed = ?").
-		WithArgs(isCompleted).
-		WillReturnRows(rows)
-
-	tasks, err := database.GetTasksByCompletion(isCompleted)
-	if err != nil {
-		t.Errorf("could not fetch tasks by completion: %v", err)
-	}
-
-	fetchCount := len(tasks)
-	if fetchCount != rowCount {
-		t.Errorf("expected %v tasks by completion, but got %v", rowCount, fetchCount)
-	}
-}
-
-func addRandomRows(rows *sqlmock.Rows, base Task) int {
-	min := 10
-	max := 30
-	rand.Seed(time.Now().UnixNano())
-	count := rand.Intn(max-min+1) + min
-
-	for i := 0; i < count; i++ {
-		rows.AddRow(i, base.Name+fmt.Sprint(i), base.Completed)
-	}
-
-	return count
+	return &Database{db}
 }
